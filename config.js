793cfd52e9fe6c33363366af8d749e9f59dc5d22
omusegad <omusegad@gmail.com>
2021-01-07 07:21:10 +0300

    env config filr added

---
+++ config.js
@@ -0,0 +1,14 @@
+const env = proccess.env;
+const config = {
+    db: { 
+        host: env.DB_HOST || 'remotemysql.com',
+        user: env.DB_USER || '2ZE90yGC6G',
+        password: env.DB_PASSWORD || 'JZFqXibSmX',
+        database: env.DB_NAME || '2ZE90yGC6G',
+        waitForConnections: true,
+        connectionLimit: env.DB_CONN_LIMIT || 2,
+        queueLimit: 0,
+        debug: env.DB_DEBUG || false
+      },
+      listPerPage: env.LIST_PER_PAGE || 10,
+};
